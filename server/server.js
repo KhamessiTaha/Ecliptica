862d4adef35b0c0bdc29e6b6314c021c1ff90a45
Taha Khamessi <taha.khamessi@gmail.com>
2024-12-11 20:31:06 +0100

    MongoDB Connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
@@ -10,10 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// Connect to MongoDB
+connectDB();
 
 // Test Route
 app.get("/", (req, res) => res.send("Ecliptica backend is live!"));
